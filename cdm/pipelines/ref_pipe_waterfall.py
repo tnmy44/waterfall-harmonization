eda01586f5cb1ee6081faa6ff17be8e61171a763
tnmy44@gmail.com <tnmy44@gmail.com>
2025-11-30 19:29:48 +0000

    AI: Added transformation to reformat 'As of date' column to yyyymmdd format after gsmbs_raw_source gem

--- cdm/pipelines/ref_pipe_waterfall.py
+++ cdm/pipelines/ref_pipe_waterfall.py
@@ -1,11 +1,11 @@
 with DAG():
-    gsmbs_raw_source = Task(
-        task_id = "gsmbs_raw_source", 
-        component = "Dataset", 
-        table = {"name" : "gsmbs_raw_source", "sourceType" : "Table", "sourceName" : "waterfall_harmonizer_cdm", "alias" : ""}
-    )
     gsmbs_expected_output = Task(
         task_id = "gsmbs_expected_output", 
         component = "Dataset", 
         table = {"name" : "gsmbs_expected_output", "sourceType" : "Table", "sourceName" : "waterfall_harmonizer_cdm", "alias" : ""}
+    )
+    ref_pipe_waterfall__as_of_date_yyyymmdd_reformat = Task(
+        task_id = "ref_pipe_waterfall__as_of_date_yyyymmdd_reformat", 
+        component = "Model", 
+        modelName = "ref_pipe_waterfall__as_of_date_yyyymmdd_reformat"
     )
