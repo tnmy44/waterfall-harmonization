77dbb257ab5d3517f5c9b37a9dca41396837b1f0
tnmy44@gmail.com <tnmy44@gmail.com>
2025-12-03 03:48:59 +0000

    Remove `ref_pipe_waterfall__sqlstatement_1` transformation and its logic from pipeline

--- cdm/pipelines/ref_pipe_waterfall.py
+++ cdm/pipelines/ref_pipe_waterfall.py
@@ -1,11 +1,2 @@
 with DAG():
-    gsmbs_expected_output = Task(
-        task_id = "gsmbs_expected_output", 
-        component = "Dataset", 
-        table = {"name" : "gsmbs_expected_output", "sourceType" : "Table", "sourceName" : "waterfall_harmonizer_cdm", "alias" : ""}
-    )
-    ref_pipe_waterfall__sqlstatement_1 = Task(
-        task_id = "ref_pipe_waterfall__sqlstatement_1", 
-        component = "Model", 
-        modelName = "ref_pipe_waterfall__sqlstatement_1"
-    )
+    pass
