a99940e7129b21e01aa7f9272ab229068266d444
tnmy44@gmail.com <tnmy44@gmail.com>
2026-01-07 17:01:34 +0000

    Fix equality and None checks in `assert` SQL test and update test metadata

--- cdm/tests/generic/assert.sql
+++ cdm/tests/generic/assert.sql
@@ -1,7 +1,7 @@
 {% test assert(model, expression, column_name) %}
 {% set column_list = '*' if should_store_failures() else "1" %}
-{% set expression_final = 'false' if expression = '' else expression %}
-{% set column_name_final = '1' if column_name = None else column_name %}
+{% set expression_final = 'false' if expression == '' else expression %}
+{% set column_name_final = '1' if column_name == None else column_name %}
 
 select
     {{ column_list }}
